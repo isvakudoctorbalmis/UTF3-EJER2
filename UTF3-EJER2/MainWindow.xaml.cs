ebc02079b582a44812e1f36b5bc1f6b2b9b11763
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-10-27 21:25:01 +0100

    Agregar archivos de proyecto.

---
+++ UTF3-EJER2/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows;
+
+namespace UTF3_EJER2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
